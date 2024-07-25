5977234f403051363403892829f3fdd74d86a8de
Ayush Chauhan <ayushc2904@gmail.com>
2024-07-26 00:49:44 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*html"],
+  theme: {
+    extend: {},
+    screens: {
+      'phone' : '450px',
+      
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+
+      '2xl': '1536px',
+    },
+  },
+  plugins: [],
+}
+
